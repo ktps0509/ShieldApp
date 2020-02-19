cb37179c1a388cd919a07dedc7786e2aec270238
ktpskittipich <ktpskittipich@gmail.com>
2020-02-20 01:56:43 +0700

    Mock Up Design Info Page

---
+++ src/app/info/Info.page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-Info',
+  templateUrl: 'Info.page.html',
+  styleUrls: ['Info.page.scss']
+})
+export class InfoPage {
+
+  constructor() {}
+
+}
